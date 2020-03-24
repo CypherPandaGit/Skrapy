065cef3bba4225adb7820e77f4738d2abcf5d996
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-24 10:30:11 +0100

    s.post edited

---
+++ test_file.py
@@ -0,0 +1,24 @@
+import requests
+import lxml.html
+from bs4 import BeautifulSoup
+
+s = requests.session()
+
+login = s.get('https://nakup.itesco.cz/groceries/cs-CZ/login?from=https%3A%2F%2Fnakup.itesco.cz%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224.991395514.1584977092-2139990256.1584977092')
+login_html = lxml.html.fromstring(login.text)
+
+hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
+form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
+
+print(form)
+
+form['email'] = 'diviline@gmail.com'
+form['password'] = 'FVxvaVgirN%!77y'
+response = s.post('https://nakup.itesco.cz/groceries/cs-CZ/login?from=https%3A%2F%2Fnakup.itesco.cz%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224.991395514.1584977092-2139990256.1584977092', data=form)
+
+print(response.url)
+
+if 'Roman' in response.text:
+    print('True')
+else:
+    print('Sorry')
