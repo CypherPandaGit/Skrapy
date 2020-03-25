82031bf757485d10190476a531f15de6959a446b
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-25 16:27:08 +0100

    added new functions, parameters and many more

--- test_file.py
+++ test_file.py
@@ -1,16 +1,35 @@
 import requests
 import lxml.html
 import json
-import http.cookiejar as cookie
-from bs4 import BeautifulSoup
+from datetime import date
+from datetime import timedelta
+
+
 
 def log_writer(my_string):
     with open('date.txt','a') as log_file:
         log_file.write(my_string + '\n')
 
+
+TUESDAY = 1  # it begins from ZERO
+
+def get_closest_tuesday(for_date):
+    tuesday = None
+    weekday = for_date.weekday()
+
+    if (weekday == TUESDAY):
+        tuesday = for_date
+    else:
+        to_add = TUESDAY - for_date.weekday()
+        tuesday = for_date + timedelta(days=to_add)
+
+    return tuesday
+
+
 s = requests.session()
 
-login = s.get('https://nakup.itesco.cz/groceries/cs-CZ/login?from=https%3A%2F%2Fnakup.itesco.cz%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224.991395514.1584977092-2139990256.1584977092')
+login = s.get('https://nakup.itesco.cz/groceries/cs-CZ/login?from=https%3A%2F%2Fnakup.'
+              'itesco.cz%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224.991395514.1584977092-2139990256.1584977092')
 login_html = lxml.html.fromstring(login.text)
 
 hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
@@ -18,49 +37,31 @@ form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
 
 print(form)
 
-form['email'] = 'EMAILHERE'
-form['password'] = 'PASSHERE'
-# response = s.post('https://nakup.itesco.cz/groceries/cs-CZ/login?from=https%3A%2F%2Fnakup.itesco.cz%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224.991395514.1584977092-2139990256.1584977092', data=form)
-#
-#
-# print('-' * 20)
-# print('RESPONSE URL: ')
-# print(response.url)
-# print('STATUS CODE: ')
-# print(response.status_code)
-# print('-' * 20 + '\n')
-# print('RESPONSE COOKIES: ')
-# print(response.cookies.get_dict())
-# print('-' * 20 + '\n')
-#
-#
-# if response.status_code == 200:
-#     print('Oh boy, you are in!\n')
-
-def data_mining(date_list):
-    response = s.post(
-        'https://nakup.itesco.cz/groceries/cs-CZ/login?from=https%3A%2F%2Fnakup.itesco.cz%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224.991395514.1584977092-2139990256.1584977092',
-        data=form)
-
-    print('-' * 20)
-    print('RESPONSE URL: ')
-    print(response.url)
-    print('STATUS CODE: ')
-    print(response.status_code)
-    print('-' * 20 + '\n')
-    print('RESPONSE COOKIES: ')
-    print(response.cookies.get_dict())
-    print('-' * 20 + '\n')
-
-    if response.status_code == 200:
-        print('Oh boy, you are in!\n')
+form['email'] = 'YOURMAILHERE'
+form['password'] = 'YOURPASSHERE'
+response = s.post('https://nakup.itesco.cz/groceries/cs-CZ/login?from=https%3A%2F%2Fnakup.'
+                  'itesco.cz%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224.991395514.1584977092-2139990256.1584977092', data=form)
 
 
+print('-' * 20)
+print('RESPONSE URL: ')
+print(response.url)
+print('STATUS CODE: ')
+print(response.status_code)
+print('-' * 20 + '\n')
+print('RESPONSE COOKIES: ')
+print(response.cookies.get_dict())
+print('-' * 20 + '\n')
 
-    response = s.get('https://nakup.itesco.cz/groceries/cs-CZ/slots/delivery/2020-{}?slotGroup=2'.format(date_list),
+
+if response.status_code == 200:
+    print('Oh boy, you are in!\n')
+
+
+def data_mining(date_list, response=response):
+
+    response = s.get('https://nakup.itesco.cz/groceries/cs-CZ/slots/delivery/{0}?slotGroup=2'.format(date_list),
                      cookies=response.cookies.get_dict(), headers={'accept': 'application/json'})
-    # response = s.get('https://nakup.itesco.cz/groceries/cs-CZ/slots/delivery/2020-03-31?slotGroup=2', cookies=response.cookies.get_dict(), headers={'accept': 'application/json'})
-
 
     if response.status_code == 200:
         print('Bruh, it looks good.\n')
@@ -73,9 +74,9 @@ def data_mining(date_list):
         for x in slots:
             # print(x['status'])
             if x['status'] != 'UnAvailable':
-                # print('Send mail bruh. Presto.')
-                date = x['start']
-                formated_date = date[0:10] + ', ' + date[10:]
+                # If AVAILABLE send mail bruh. Presto! Yeah and put the data in my TXT file.
+                mined_date = x['start']
+                formated_date = mined_date[0:10] + ', ' + mined_date[10:]
 
                 for i in range(1):
                     new_string = 'Available: {0}'.format(str(formated_date))
@@ -86,13 +87,7 @@ def data_mining(date_list):
     return 'end'
 
 
-
-date_list = ['03-24', '03-31', '04-07', '04-14', '04-21']
-print(data_mining([0]))
-print(data_mining([1]))
-print(data_mining([2]))
-
-# else:
-#     print('Sorry, you messed up again... Bruh...')
-
-# print(response.status_code)
+print(data_mining(get_closest_tuesday(date.today())))
+print(data_mining(get_closest_tuesday(date.today() + timedelta(days=7))))
+print(data_mining(get_closest_tuesday(date.today() + timedelta(days=14))))
+print(data_mining(get_closest_tuesday(date.today() + timedelta(days=21))))
