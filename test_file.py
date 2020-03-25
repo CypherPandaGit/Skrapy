2acb6f4ca4be272d2d8db239879bff13119be8c2
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-25 20:45:30 +0100

    added main() func

--- test_file.py
+++ test_file.py
@@ -5,19 +5,60 @@ from datetime import date
 from datetime import timedelta
 
 
+def main():
+    s = requests.session()
+
+    login = s.get('https://nakup.itesco.cz/groceries/cs-CZ/'
+                  'login?from=https%3A%2F%2Fnakup.itesco.cz'
+                  '%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224'
+                  '.991395514.1584977092-2139990256.1584977092')
+    login_html = lxml.html.fromstring(login.text)
+
+    hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
+    form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
+
+    print(form)
+
+    form['email'] = 'YOUR_MAIL_HERE'
+    form['password'] = 'YOUR_PASSWORD_HERE'
+    response = s.post('https://nakup.itesco.cz/groceries/cs-CZ/'
+                      'login?from=https%3A%2F%2Fnakup.itesco.cz'
+                      '%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224'
+                      '.991395514.1584977092-2139990256.1584977092',
+                      data=form)
+
+    print('-' * 20)
+    print('RESPONSE URL: ')
+    print(response.url)
+    print('STATUS CODE: ')
+    print(response.status_code)
+    print('-' * 20 + '\n')
+    print('RESPONSE COOKIES: ')
+    print(response.cookies.get_dict())
+    print('-' * 20 + '\n')
+
+    if response.status_code == 200:
+        print('Oh boy, you are in!\n')
+
+    print(data_mining(get_closest_tuesday(date.today()), response, s))
+    print(data_mining(get_closest_tuesday(date.today() + timedelta(days=7)), response, s))
+    print(data_mining(get_closest_tuesday(date.today() + timedelta(days=14)), response, s))
+    print(data_mining(get_closest_tuesday(date.today() + timedelta(days=21)), response, s))
+
 
 def log_writer(my_string):
-    with open('date.txt','a') as log_file:
+    with open('date.txt', 'a') as log_file:
         log_file.write(my_string + '\n')
 
 
 TUESDAY = 1  # it begins from ZERO
 
+
 def get_closest_tuesday(for_date):
     tuesday = None
     weekday = for_date.weekday()
 
-    if (weekday == TUESDAY):
+    if weekday == TUESDAY:
         tuesday = for_date
     else:
         to_add = TUESDAY - for_date.weekday()
@@ -26,45 +67,7 @@ def get_closest_tuesday(for_date):
     return tuesday
 
 
-s = requests.session()
-
-login = s.get('https://nakup.itesco.cz/groceries/cs-CZ/'
-              'login?from=https%3A%2F%2Fnakup.itesco.cz'
-              '%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224'
-              '.991395514.1584977092-2139990256.1584977092')
-login_html = lxml.html.fromstring(login.text)
-
-hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
-form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
-
-print(form)
-
-form['email'] = 'MAILHERE'
-form['password'] = 'PASSHERE'
-response = s.post('https://nakup.itesco.cz/groceries/cs-CZ/'
-                  'login?from=https%3A%2F%2Fnakup.itesco.cz'
-                  '%2Fgroceries%2Fcs-CZ%3F_ga%3D2.255516224'
-                  '.991395514.1584977092-2139990256.1584977092',
-                  data=form)
-
-
-print('-' * 20)
-print('RESPONSE URL: ')
-print(response.url)
-print('STATUS CODE: ')
-print(response.status_code)
-print('-' * 20 + '\n')
-print('RESPONSE COOKIES: ')
-print(response.cookies.get_dict())
-print('-' * 20 + '\n')
-
-
-if response.status_code == 200:
-    print('Oh boy, you are in!\n')
-
-
-def data_mining(date_list, response=response):
-
+def data_mining(date_list, response, s=None):
     response = s.get('https://nakup.itesco.cz/groceries/cs-CZ/slots/'
                      'delivery/{0}?slotGroup=2'.format(date_list),
                      cookies=response.cookies.get_dict(),
@@ -91,10 +94,9 @@ def data_mining(date_list, response=response):
 
     else:
         print('Sorry, you messed up again... Bruh...')
-    return 'end'
+
+    return '>>>>> END OF DATA MINING ROUND <<<<<'
 
 
-print(data_mining(get_closest_tuesday(date.today())))
-print(data_mining(get_closest_tuesday(date.today() + timedelta(days=7))))
-print(data_mining(get_closest_tuesday(date.today() + timedelta(days=14))))
-print(data_mining(get_closest_tuesday(date.today() + timedelta(days=21))))
+if __name__ == '__main__':
+    main()
